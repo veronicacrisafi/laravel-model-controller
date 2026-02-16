32ac9301abac6f66d1b03eac348219dbba507cef
veronicacrisafi <veronicacrisafi3@gmail.com>
2026-02-16 10:14:33 +0100

    installato bootstrap

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
     <title>Laravel-Model-Controller</title>
 </head>
 
