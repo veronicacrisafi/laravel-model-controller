9893312a275e9dd24c46aa58e0f9f148428c3db1
veronicacrisafi <veronicacrisafi3@gmail.com>
2026-02-16 10:46:49 +0100

    Aggiunta la visualizzazione dei film nella vista index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,1 +1,23 @@
 @extends('layouts.master')
+
+<div>
+    <div class="row">
+        @foreach ($movies as $movie)
+            <div class="col">
+                <h4>
+                    {{ $movie['title'] }}
+                </h4>
+                <h6>
+                    {{ $movie['original_title'] }}
+                </h6>
+                <p>
+                    {{ $movie['nationality'] }}
+                    {{ $movie['date'] }}
+                </p>
+                <section>
+                    {{ $movie['vote'] }}
+                </section>
+            </div>
+        @endforeach
+    </div>
+</div>
