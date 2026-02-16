729166224ce0b2a3aa211d814c95793a45a09cec
veronicacrisafi <veronicacrisafi3@gmail.com>
2026-02-16 11:02:01 +0100

    Ristrutturata la vista index per utilizzare il layout con card per i film
    concesso l'utilizzo della section('content') al layout master.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,23 +1,27 @@
 @extends('layouts.master')
 
-<div>
-    <div class="row">
-        @foreach ($movies as $movie)
-            <div class="col">
-                <h4>
-                    {{ $movie['title'] }}
-                </h4>
-                <h6>
-                    {{ $movie['original_title'] }}
-                </h6>
-                <p>
-                    {{ $movie['nationality'] }}
-                    {{ $movie['date'] }}
-                </p>
-                <section>
-                    {{ $movie['vote'] }}
-                </section>
-            </div>
-        @endforeach
+@section('content')
+    <div>
+        <div class="row">
+            @foreach ($movies as $movie)
+                <div class="col-4 mb-4">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">
+                                {{ $movie['title'] }}
+                            </h5>
+                            <h6 class="card-subtitle mb-2 text-muted">
+                                {{ $movie['original_title'] }}
+                            </h6>
+                            <div class="card-text">
+                                {{ $movie['nationality'] }}<br>
+                                {{ $movie['date'] }}<br>
+                                {{ $movie['vote'] }}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
     </div>
-</div>
+@endsection
